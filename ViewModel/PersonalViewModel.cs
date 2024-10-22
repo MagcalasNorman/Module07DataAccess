3205c5fff0f74953c027068f72e821f30b04b561
MagcalasNorman <magcalas.normanmiguel@auf.edu.ph>
2024-10-22 09:07:44 +0800

    Add project files.

---
+++ ViewModel/PersonalViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module07DataAccess.Model;
+using Module07DataAccess.Services;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Module07DataAccess.ViewModel
+{
+    public class PersonalViewModel : INotifyPropertyChanged
+    {
+        private readonly PersonalService _personalService;
+        public ObservableCollection<Personal> PersonalList { get; set; }
+
+        private bool _isBusy;
+        public bool isBusy
+        {
+            get => _isBusy;
+            set { _isBusy = value; OnPropertyChanged(); }
+        }
+
+        private string _statusMessage;
+        public string statusMessage
+        {
+            get => _statusMessage;
+            set { _statusMessage = value; OnPropertyChanged(); }
+        }
+
+        public ICommand LoadDataCommand { get; }
+
+        public PersonalViewModel()
+        {
+            _personalService = new PersonalService();
+            PersonalList = new ObservableCollection<Personal>();
+            LoadDataCommand = new Command(async () => await LoadData());
+
+            LoadData();
+        }
+
+        public async Task LoadData()
+        {
+            if (isBusy) return;
+            isBusy = true;
+            statusMessage = "Loading personal data...";
+            try
+            {
+                var personals = await _personalService.GetAllPersonalsAsync();
+                PersonalList.Clear();
+                foreach (var personal in personals) 
+                {
+                    PersonalList.Add(personal);
+                }
+                statusMessage = "Data Load Successfully";
+            }
+            catch (Exception ex)
+            {
+                statusMessage = $"Failed to Load data:{ex.Message}";
+            }
+            finally
+            {
+                isBusy = false;
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
